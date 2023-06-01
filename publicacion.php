f3f376aedcab5b1f727616fe38617cfe1a21fd78
Your Name <david.maldonadolv@uanl.edu.mx>
2023-05-31 21:16:53 -0600

    Crear publicacion

---
+++ publicacion.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+    <link rel="stylesheet" href="CSS/registro-style.css">
+    <title>Crear publicaciones</title>
+</head>
+<body>
+    <!-- MENÚ DE NAVEGACIÓN -->
+<nav>
+    
+    <div>
+    <a href="index.html"></a><img src="img/logo.jpg" alt="">
+    </div>
+
+    <div class="enlaces">
+        <a href="index.html">Inicio</a>
+        <a href="crear.php">Crear publicación</a>
+        <a href="mis-publicaciones.html">Mis publicaciones</a>
+        <a href="inicio.php">Iniciar Sesión</a>
+        <a class="input-container">
+            <input type="text" placeholder="Buscar">
+            <i class="fas fa-search"></i>
+        </a>
+    </div>
+</nav>
+<!-- CREAR PUBLICACION -->
+<section>
+    <div class="container">
+    <form class="form"action="publicar.php" method="post" enctype="multipart/form-data">
+  <div class="input-box w50">
+    <input type="text" name="Titulo" required>
+    <span>Titulo de la publicacion (s)</span>
+  </div>
+  
+  <div class="input-box w50">
+    <input type="text" name="Descripcion" required>
+    <span>Descripción</span>
+  </div>
+  <div class="imagen">
+    Selecciona una imagen para subir:
+    <input type="file" name="imagen" id="imagen">
+  </div>
+  <div class="input-box w100 btn">
+    <input type="submit" value="Enviar">
+  </div>
+</form>
+    </div>
+</section>
+
+</body>
+</html>
---
+++ publicar.php
@@ -0,0 +1,33 @@
+<?php
+// Conectarse a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "proyectof";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Obtener los datos del formulario
+$Titulo = $_POST['Titulo'];
+$Descripcion = $_POST['Descripcion'];
+$imagen = $_FILES['imagen']['tmp_name'];
+
+// Convertir la imagen en binario
+$imagen_binario = addslashes(file_get_contents($imagen));
+
+// Insertar los datos en la tabla
+$sql = "INSERT INTO publicacion (IdUsuario, Titulo, Descripcion, Imagen, Visibilidad) VALUES ('1',  '$Titulo', '$Descripcion', '$imagen_binario','1')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Los datos se han insertado correctamente.";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
